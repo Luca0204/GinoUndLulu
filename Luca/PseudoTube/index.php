431b76e66f163136cf514ae2729aef20a583b730
Luca0204 <luca0204@freenet.de>
2017-12-18 16:31:59 +0100

    Lalige Sos

---
+++ Luca/PseudoTube/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+        <title>PseudoTube</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <style media="screen"></style>
+  </head>
+  <body>
+    <!-- TOPBAR -->
+    <div class="TopBar">
+      <div class="Search">
+
+       <form class="" action="index.html" method="post">
+         <input placeholder="Suchen..." type="text" name="SearchText" class="SearchText" value="">
+         <button type="button" name="SearchBTN" class="SearchBTN">Suchen</button>
+       </form>
+    </div>
+    </div>
+  </body>
+</html>
