1e142ff9924d6e26c2bd58ed1da16ae529b0ba2a
Luca0204 <luca0204@freenet.de>
2017-12-18 21:37:16 +0100

    Player Update

--- Luca/PseudoTube/index.php
+++ Luca/PseudoTube/index.php
@@ -8,16 +8,83 @@
         <link rel="stylesheet" href="css/style.css">
         <style media="screen"></style>
   </head>
+  <!-- TOPBAR -->
   <body>
-    <!-- TOPBAR -->
-    <div class="TopBar">
-      <div class="Search">
+  <div class='TopBar'>
+    <a href="../PseudoTube/">
+<img class="Logo" width="40" height="40" src='img/Logo.png' alt='Video Picture'></a>
+    <div class='Search'>
 
-       <form class="" action="index.html" method="post">
-         <input placeholder="Suchen..." type="text" name="SearchText" class="SearchText" value="">
-         <button type="button" name="SearchBTN" class="SearchBTN">Suchen</button>
-       </form>
+     <form class='' action='index.php' method='get'>
+       <input placeholder='Suchen...' type='text' name='s' class='SearchText' value=''>
+       <button type='submit' class='SearchBTN'>Suchen</button>
+     </form>
+  </div>
+  </div>
+  <?php
+  if (empty($_GET[video])) {
+
+
+
+    echo "
+
+
+    <!-- Content -->
+
+    <div class='Content'>";
+    $db_link = mysqli_connect (localhost,root,raspberry,PseudoTube);
+
+if (isset($_GET[s])) {
+  $sql = "SELECT * FROM Videos WHERE Video_Titel = '".$_GET[s]."'";
+}
+else {
+  $sql = "SELECT * FROM Videos ORDER BY RAND() LIMIT 20";
+}
+$db_erg = mysqli_query( $db_link, $sql );
+if ( ! $db_erg )
+{
+die('Fehler1' . mysqli_error());
+}
+
+while ($Video = mysqli_fetch_array( $db_erg, MYSQL_ASSOC))
+{
+  echo "<a style='color: white; text-decoration: none;' href='../PseudoTube/?video=".$Video['Video_ID']."'><div class='Video'><img src='img/Video.png' alt='Video Picture'><br>".$Video['Video_Titel']."</a></div>";
+}
+echo "
     </div>
-    </div>
+
   </body>
+  ";
+  }
+
+  elseif (isset($_GET[video])) {
+  echo "
+    <!-- Player -->
+    <div class='Content'>";
+
+    $video = $_GET[video];
+    $db_link = mysqli_connect (localhost,root,raspberry,PseudoTube);
+    $sql = "SELECT * FROM Videos WHERE Video_ID = '".$video."'";
+
+    $db_erg = mysqli_query( $db_link, $sql );
+    if ( ! $db_erg )
+    {
+    die('Fehler2' . mysqli_error());
+    }
+
+    while ($Video = mysqli_fetch_array( $db_erg, MYSQL_ASSOC))
+    {
+      echo "<video style='background-color:rgb(10, 10, 10);' width='1280' height='720' controls>
+  <source src='Videos/".$Video['FileName']."' type='video/mp4'>
+  Ja nö deim broswer nix können das
+</video>
+<br>
+".$Video['Video_Titel']."
+";
+    }
+
+    echo "</div>";
+  }
+  ?>
+
 </html>
